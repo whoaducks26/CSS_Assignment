e0e87c67f2179ff26ad9bb49ffbf385869e0c627
Clyde Kenji Koh <clydekoh777@gmail.com>
2025-02-09 23:04:19 +0800

    Delete app/next.config.js

--- app/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['instagram.fsin15-1.fna.fbcdn.net', 'instagram.fsin15-2.fna.fbcdn.net'],
-    },
-};
-
-module.exports = nextConfig
